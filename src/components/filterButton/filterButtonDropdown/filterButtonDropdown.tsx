b557c3f2db4808b7a1204c2fd0789eb86962bc76
duckduckcody <cody.hennighan@gmail.com>
2022-10-02 13:39:13 +0800

    cahnges

--- src/components/filterButton/filterButtonDropdown/filterButtonDropdown.tsx
+++ src/components/filterButton/filterButtonDropdown/filterButtonDropdown.tsx
@@ -1,7 +1,15 @@
-import { autoUpdate, flip, shift, useFloating } from '@floating-ui/react-dom';
+import {
+  autoUpdate,
+  flip,
+  shift,
+  useDismiss,
+  useFloating,
+  useInteractions,
+} from '@floating-ui/react-dom-interactions';
 import { FC, useMemo } from 'react';
 import styled from 'styled-components';
 import ChevronIcon from '../../../icons/chevron.svg';
+import { useStore } from '../../../store/useStore';
 import { FilterButtonContainer } from '../filterButton.shared';
 import { FilterButtonCrossButton } from '../filterButtonCrossButton';
 import { FilterButtonDropdownMenu } from './filterButtonDropdownMenu/filterButtonDropdownMenu';
@@ -34,11 +42,21 @@ export const FilterButtonDropdown: FC<FilterButtonDropdownProps> = ({
   onValueClear = () => undefined,
   onButtonClick = () => undefined,
 }) => {
-  const { x, y, reference, floating, strategy } = useFloating({
+  const setActiveFilterId = useStore((state) => state.filters.setActiveFilerId);
+
+  const { x, y, reference, floating, strategy, context } = useFloating({
     middleware: [flip(), shift()],
     placement: 'bottom-start',
     whileElementsMounted: autoUpdate,
+    onOpenChange: () => {
+      console.log('testing');
+      setActiveFilterId('');
+    },
   });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    useDismiss(context),
+  ]);
 
   const valueString = useMemo(() => {
     if (Array.isArray(selectedOptions)) {
@@ -52,6 +70,7 @@ export const FilterButtonDropdown: FC<FilterButtonDropdownProps> = ({
     <>
       <FilterButtonContainer
         ref={reference}
+        {...getReferenceProps()}
         active={active}
         hasValues={Boolean(valueString)}
         onClick={onButtonClick}
@@ -76,6 +95,7 @@ export const FilterButtonDropdown: FC<FilterButtonDropdownProps> = ({
             top: y ?? 0,
             left: x ?? 0,
           }}
+          {...getFloatingProps()}
         >
           <FilterButtonDropdownMenu
             type={menuType}
